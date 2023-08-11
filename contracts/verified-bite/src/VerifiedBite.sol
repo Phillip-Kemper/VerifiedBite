0f38d09339f707910d705f2fef67278544b541ae
phillip-kemper <phillip-kemper@gmx.net>
2023-08-11 22:51:25 +0200

    add admin to verified bite for adding receipt code

--- contracts/verified-bite/src/VerifiedBite.sol
+++ contracts/verified-bite/src/VerifiedBite.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.13;
 
 contract VerifiedBite {
+    address public admin;
     mapping(uint256 => Review[]) public submittedReviews;
     mapping(bytes32 => uint256) public unusedReceiptCodes;
 
@@ -15,7 +16,13 @@ contract VerifiedBite {
         _;
     }
 
-    constructor() {
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only admin can call this function");
+        _;
+    }
+
+    constructor(address _admin) {
+        admin = _admin;
         uint256 userId1 = 1234567890123456789012345678901234567890;
         uint256 userId2 = 2345678901234567890123456789012345678901;
         uint256 userId3 = 3456789012345678901234567890123456789012;
@@ -25,7 +32,7 @@ contract VerifiedBite {
         submittedReviews[userId3].push(Review(0x6CbAa746E1F17804E9b2b81222C9f4a0A50a64A9, 3));
     }
 
-    function addReceiptCode(bytes32 receiptCode, uint256 restaurantId) public {
+    function addReceiptCode(bytes32 receiptCode, uint256 restaurantId) public onlyAdmin() {
         unusedReceiptCodes[receiptCode] = restaurantId;
     }
 
