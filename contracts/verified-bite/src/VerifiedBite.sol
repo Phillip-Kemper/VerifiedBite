61c138bf690139c5675822dc71817fc32335a75a
phillip-kemper <phillip-kemper@gmx.net>
2023-08-11 21:08:04 +0200

    init state variables, rating modifier, wip review function

--- contracts/verified-bite/src/VerifiedBite.sol
+++ contracts/verified-bite/src/VerifiedBite.sol
@@ -2,4 +2,25 @@
 pragma solidity ^0.8.13;
 
 contract VerifiedBite {
+    mapping(uint256 => Review[]) public submittedReviews;
+    mapping(bytes32 => uint256) public unusedReceiptCodes;
+
+    struct Review {
+        uint256 restaurantId;
+        uint256 rating;
+    }
+
+    modifier validRating(uint256 rating) {
+        require(rating >= 1 && rating <= 5, "Rating must be between 1 and 5");
+        _;
+    }
+
+
+    function submitReview(bytes32 receiptCode, uint256 rating) public {
+        // is receipt code valid?
+        // what restaurant is it for?
+        uint256 restaurantId= 10;
+        submittedReviews[restaurantId].push(Review(restaurantId, rating));
+    } 
+
 }
