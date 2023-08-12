07a13c41eeec40c9bc05568ab1ae984a765bc837
alex <alexander.abstreiter@gmail.com>
2023-08-12 14:25:43 +0200

    add images and names to restaurants

--- client/constants/restaurants.ts
+++ client/constants/restaurants.ts
@@ -7,10 +7,10 @@ interface Restaurant {
 
 export const restaurants: Array<Restaurant> = [
     {id: 1, name: "Star Pizza", imageURL: "https://source.unsplash.com/60nzTP7_hMQ"},
+    {id: 4, name: "Top Pho", imageURL: "https://source.unsplash.com/NFQi_2HUNRI"},
     {id: 2, name: "Burger Palace", imageURL: "https://source.unsplash.com/I7A_pHLcQK8"},
-    {id: 3, name: "Burger Palace", imageURL: "https://source.unsplash.com/I7A_pHLcQK8"},
-    {id: 4, name: "Burger Palace", imageURL: "https://source.unsplash.com/I7A_pHLcQK8"},
-    {id: 5, name: "Burger Palace", imageURL: "https://source.unsplash.com/I7A_pHLcQK8"},
+    {id: 5, name: "Elite Sushi", imageURL: "https://source.unsplash.com/O2yNzXdqOu0"},
+    {id: 3, name: "Wiener Stuben", imageURL: "https://source.unsplash.com/MyfbM2QYF4o"},
 ]
 
 export const getRestaurantById = (id: number) => {
