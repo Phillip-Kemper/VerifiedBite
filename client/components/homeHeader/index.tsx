39644c38b4d79b0e47a0321918c231f2976f4795
korabs-x <ko.abstreiter@gmail.com>
2023-08-12 09:17:45 +0200

    make header a link

--- client/components/homeHeader/index.tsx
+++ client/components/homeHeader/index.tsx
@@ -1,14 +1,16 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Link, Typography } from "@mui/material";
 import FastfoodIcon from "@mui/icons-material/Fastfood";
 
 const HomeHeader = () => {
   return (
-    <Box sx={{ display: "flex", border: 1, justifyContent: "center", mt: 3 }}>
-      <Typography variant="h2" component="div">
-        <FastfoodIcon fontSize="large" />
-        Verified Bite
-      </Typography>
-    </Box>
+    <Link href="/" sx={{ textDecoration: "none", color: "inherit" }}>
+      <Box sx={{ display: "flex", border: 1, justifyContent: "center", mt: 3 }}>
+        <Typography variant="h2" component="div">
+          <FastfoodIcon fontSize="large" />
+          Verified Bite
+        </Typography>
+      </Box>
+    </Link>
   );
 };
 
