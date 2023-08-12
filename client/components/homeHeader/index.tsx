939a6afab245422b0ea8b4bae2c378c28a6d8139
korabs-x <ko.abstreiter@gmail.com>
2023-08-12 16:37:35 +0200

    design

--- client/components/homeHeader/index.tsx
+++ client/components/homeHeader/index.tsx
@@ -4,21 +4,20 @@ import FastfoodIcon from "@mui/icons-material/Fastfood";
 const HomeHeader = () => {
   return (
     <Link href="/" sx={{ textDecoration: "none", color: "inherit" }}>
-      <Box sx={{ border: 1, justifyContent: "center", mt: 5, borderRadius: 2, padding: 1}}>
-      <Box sx={{ display: "flex", justifyContent: "center"}}>
-        <Typography variant="h2" component="div" >
-          <FastfoodIcon fontSize="large" />
-          Verified Bite
-          <FastfoodIcon fontSize="large" />
-        </Typography>
-      </Box>
+      <Box sx={{ border: 1, justifyContent: "center", mt: 5, borderRadius: 2, padding: 1, background: "floralwhite" }}>
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <Typography variant="h2" component="div">
+            <FastfoodIcon fontSize="large" />
+            Verified Bite
+            <FastfoodIcon fontSize="large" />
+          </Typography>
+        </Box>
 
-      <Box sx={{ display: "flex", justifyContent: "center"}}>
-        <Typography variant='h4'>Don't Trust Your Food. Verify It!</Typography>
-      </Box>
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <Typography variant="h4">Don't Trust Your Food. Verify It!</Typography>
+        </Box>
       </Box>
     </Link>
-
   );
 };
 
