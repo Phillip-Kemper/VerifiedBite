5a46f94abc39f93463a5bd7e6149b92b925b51eb
alex <alexander.abstreiter@gmail.com>
2023-08-12 11:57:25 +0200

    add restaurant detail page

---
+++ client/constants/restaurants.ts
@@ -0,0 +1,18 @@
+
+interface Restaurant {
+    id: number;
+    name: string;
+    imageURL: string; 
+}
+
+export const restaurants: Array<Restaurant> = [
+    {id: 1, name: "Burger Palace", imageURL: "https://source.unsplash.com/I7A_pHLcQK8"},
+    {id: 2, name: "Burger Palace", imageURL: "https://source.unsplash.com/I7A_pHLcQK8"},
+    {id: 3, name: "Burger Palace", imageURL: "https://source.unsplash.com/I7A_pHLcQK8"},
+    {id: 4, name: "Burger Palace", imageURL: "https://source.unsplash.com/I7A_pHLcQK8"},
+    {id: 5, name: "Burger Palace", imageURL: "https://source.unsplash.com/I7A_pHLcQK8"},
+]
+
+export const getRestaurantById = (id: number) => {
+    return restaurants.find((r) => r.id === id)
+}
